013875be6746be101929c07ad8451f237b862953
momnpa333 <82216606+momnpa333@users.noreply.github.com>
2023-09-21 20:24:31 +0900

    Init : 프로젝트 생성

---
+++ WishMate/src/main/java/midtwo/WishMate/WishMateApplication.java
@@ -0,0 +1,13 @@
+package midtwo.WishMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishMateApplication.class, args);
+	}
+
+}
---
+++ WishMate/src/test/java/midtwo/WishMate/WishMateApplicationTests.java
@@ -0,0 +1,13 @@
+package midtwo.WishMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
